c621c26817442ea8f7613eb54a506758ae8e9ec5
ArshilHapani <arshilhapani998@gmail.com>
2022-12-20 18:28:24 +0530

    20-dec-22 Routes and models initialized

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,18 @@
 const connectToMongo = require('./db');
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
 connectToMongo();
+const app = express();
+const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello Arshil!')
-})
+app.use(express.json());
+//Available routes
+app.use('/api/auth',require('./routes/auth'));
+app.use('/api/notes',require('./routes/notes'));
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
 })
+//Command to run...
+//! npx nodemon .\index.js -Y
+
+
