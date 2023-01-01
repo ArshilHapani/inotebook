70d066506366ea70760778061e28402f5690355f
ArshilHapani <arshilhapani998@gmail.com>
2023-01-01 23:03:45 +0530

    Build folder ADded

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/inotebook";
+const mongoURI = "mongodb+srv://iNotebook:Dpz3t4VaAivZXBO2@atlascluster.hmqrnbm.mongodb.net/test";
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI,()=>{
